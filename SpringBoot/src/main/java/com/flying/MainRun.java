f2a9111a20b6f0b5ff5379523990e1ff94dd5969
FlyingJiang <1078058570@qq.com>
2019-11-13 16:53:40 +0800

    工作前同步下之前的内容“ ；

--- SpringBoot/src/main/java/com/flying/MainRun.java
+++ SpringBoot/src/main/java/com/flying/MainRun.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+
 @ComponentScan(basePackages = "com.flying.controller")
 @EnableAutoConfiguration
 public class MainRun {
